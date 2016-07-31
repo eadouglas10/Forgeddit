fbae12c1fc12771e5b1f37359f51c68046561821
Erik Douglas <eadouglas10@gmail.com>
2016-07-31 14:04:57 -0400

    Can now add links and follow urls

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+
+  def view
+    @link = Link.find(params["id"])
+  end
+
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,15 @@
 class LinksController < ApplicationController
+
+  def index
+    @links = Link.all
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.create(url: params["link"]["url"])
+    redirect_to links_path
+  end
 end
